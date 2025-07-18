9fe517d7e2f3b01742b280d6b4e6cc94041f610e
BlurFeng <pop297197228@gmail.com>
2025-07-18 18:07:01 +0900

    enhanced user input demo.

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -9,6 +9,9 @@
             string lastName = string.Empty;
             int age = 0;
             int retirementAge = 65;
+            decimal salary = 0;
+            char gender = char.MinValue;
+            bool working = true;
 
             // 提示用户输入姓名和年龄。
             Console.WriteLine("Please enter your first name:");
@@ -21,13 +24,27 @@
             Console.WriteLine("Please enter your age:");
             age = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Please enter your salary:");
+            salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter your gender (M or F):");
+            gender = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Are you working? (true or false):");
+            working = Convert.ToBoolean(Console.ReadLine());
+
             // 处理数据。
             int workingYearsRemaining = retirementAge - age;
 
             // 输出结果。
             Console.WriteLine($"Full name: {firstName} {lastName}");
             Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Your Salary is: {salary}");
+            Console.WriteLine($"Your Gender is: {gender}");
+            Console.WriteLine($"You Are Employed: {working}");
             Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+
+
         }
     }
 }
