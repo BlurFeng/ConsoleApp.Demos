3f1b77582e9dc1a67d4818ee00f575b735c8c57c
BlurFeng <pop297197228@gmail.com>
2025-07-19 19:10:06 +0900

    Add Exception Handling demo and Modified the Sample Calculator.

---
+++ ConsoleApp.ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp.ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 接受用户输入的年龄并打印到屏幕上。如果收到无效输入，则显示错误消息。
+            // Write a program that takes a user's age as input and prints it to the screen. Display an error message if an invalid input is received.
+            try
+            {
+                Console.Write("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"You are {age} years old. ");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Your age value was incorrect, please try again. ");
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("********** - Thank you for using this program - ***********");
+            }
+        }
+    }
+}
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -4,94 +4,108 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******** - Welcome to the sample calculator! - ********");
-
-            // 提示用户输入操作。
-            // Show calculator options / Show menu
-            Console.WriteLine("Please select an operation (-1 to exit program) ");
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraction");
-            Console.WriteLine("3. Multiplication");
-            Console.WriteLine("4. Division");
-            Console.WriteLine("5. Fibonacci sequence");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            // Variable Declarations
+            int choice = 0;
+            int num1, num2 = 0;
 
             while (choice != -1)
             {
-                Console.Write("Please enter the first number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the second number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                // 根据用户选择的操作执行相应的计算。
-                // Decide which operation is needed based on selected option
-
-                // 使用 if 语句选择操作。
-                /*If statements */
-                //if (choice == 1)
-                //{
-                //    /* do addition */
-                //}
-                //else if (choice == 2)
-                //{
-                //    /* subtraction*/
-                //}
-                //else if (choice == 3)
-                //{
-                //    /*Multiplication*/
-                //}
-                //else if (choice == 4)
-                //{
-                //    /*Division*/
-                //}
-                //else
-                //{
-                //    Console.WriteLine("Invalid choice");
-                //}
-                // Prompt for user input
-
-                // 使用 switch 语句选择操作。
-                /* Switch statement */
-                int answer = 0;
-                switch (choice)
+                try
                 {
-                    case 1:
-                        answer = num1 + num2;
+                    Console.Clear();
+                    Console.WriteLine("******** - Welcome to the sample calculator! - ********");
+
+                    // 提示用户输入操作。
+                    // Show calculator options / Show menu
+                    Console.WriteLine("Please select an operation (-1 to exit program) ");
+                    Console.WriteLine("1. Addition");
+                    Console.WriteLine("2. Subtraction");
+                    Console.WriteLine("3. Multiplication");
+                    Console.WriteLine("4. Division");
+                    Console.WriteLine("5. Fibonacci sequence");
+
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == -1)
+                    {
                         break;
-                    case 2:
-                        answer = num1 - num2;
-                        break;
-                    case 3:
-                        answer = num1 * num2;
-                        break;
-                    case 4:
-                        answer = num1 / num2;
-                        break;
-                    case 5:
-                        for (int i = num1; i <= num2; i++)
-                        {
-                            answer += i;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice");
-                        break;
+                    }
+
+                    Console.Write("Please enter the first number: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please enter the second number: ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                    // 根据用户选择的操作执行相应的计算。
+                    // Decide which operation is needed based on selected option
+
+                    // 使用 if 语句选择操作。
+                    /*If statements */
+                    //if (choice == 1)
+                    //{
+                    //    /* do addition */
+                    //}
+                    //else if (choice == 2)
+                    //{
+                    //    /* subtraction*/
+                    //}
+                    //else if (choice == 3)
+                    //{
+                    //    /*Multiplication*/
+                    //}
+                    //else if (choice == 4)
+                    //{
+                    //    /*Division*/
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("Invalid choice");
+                    //}
+                    // Prompt for user input
+
+                    // 使用 switch 语句选择操作。
+                    /* Switch statement */
+                    int answer = 0;
+                    switch (choice)
+                    {
+                        case 1:
+                            answer = num1 + num2;
+                            break;
+                        case 2:
+                            answer = num1 - num2;
+                            break;
+                        case 3:
+                            answer = num1 * num2;
+                            break;
+                        case 4:
+                            answer = num1 / num2;
+                            break;
+                        case 5:
+                            for (int i = num1; i <= num2; i++)
+                            {
+                                answer += i;
+                            }
+                            break;
+                        default:
+                            throw new Exception("Invalid Menu Item Selected.");
+                    }
+
+                    // print output 
+                    Console.WriteLine($"The result is: {answer}");
                 }
-
-                // 输出结果。
-                // print output 
-                Console.WriteLine($"The result is: {answer}");
-                Console.WriteLine("Press Enter to continue");
-                Console.ReadLine();
-                Console.Clear();
-
-                Console.WriteLine("Please select an operation (-1 to exit program) ");
-                Console.WriteLine("1. Addition");
-                Console.WriteLine("2. Subtraction");
-                Console.WriteLine("3. Multiplication");
-                Console.WriteLine("4. Division");
-                Console.WriteLine("5. Fibonacci sequence");
-                choice = Convert.ToInt32(Console.ReadLine());
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadLine();
+                }
             }
 
             Console.WriteLine("******** - Thank you for using the sample calculator! - ********");
